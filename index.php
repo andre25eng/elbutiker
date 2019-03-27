40b6e26848211d6215966d66616131c5e7ca1bf7
andre25eng <31853449+andre25eng@users.noreply.github.com>
2019-03-27 13:25:54 +0100

    Påbörjade första sidan

--- index.php
+++ index.php
@@ -7,6 +7,24 @@
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-    <h1>gjaoilksdv </h1>
+    <div class="container">
+        <header>
+            <h1>Elektronik Butiker STHLM</h1>
+            <p>Välj butiken som passar dig bäst</p>
+            <nav>
+                <a href="#">Hem</a>
+                <a href="#">Butiker</a>
+                <a href="#">Recensioner</a>
+                <a href="#">Skapa Konto</a>
+                <a href="#">Log In</a>
+            </nav>
+            <main>
+                <div>
+                    <h2></h2>
+                    <p></p>
+                </div>
+            </main>
+        </header>
+    </div>
 </body>
 </html>
